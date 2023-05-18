79808de793b971904366f151ec4ac8f12d32dce3
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-05-17 20:03:27 -0700

    Added plugin index

---
+++ src/plugin/index.ts
@@ -0,0 +1,14 @@
+import InlineCheckbox from './InlineCheckbox.vue';
+import InlineSelect from './InlineSelect.vue';
+import InlineSwitch from './InlineSwitch.vue';
+import InlineTextField from './InlineTextField.vue';
+import InlineTextarea from './InlineTextarea.vue';
+
+
+export default {
+	InlineCheckbox,
+	InlineSelect,
+	InlineSwitch,
+	InlineTextField,
+	InlineTextarea,
+};
