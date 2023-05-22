bf35938b87cbd59bcf4a16147542a416b432de6b
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-05-21 17:14:45 -0700

    Adding component starter

---
+++ src/plugin/VInlineTextarea.vue
@@ -0,0 +1,15 @@
+<template></template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+:deep(.v-field__append-inner) {
+	padding: 0 !important;
+}
+
+.icons-container {
+	height: 100%;
+}
+</style>
