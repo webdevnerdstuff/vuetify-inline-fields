6749f0d68adb2230b1e7bd2cec88b91b7247345f
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-05-27 16:20:06 -0700

    Added all code/functionality

--- src/plugin/VInlineTextarea.vue
+++ src/plugin/VInlineTextarea.vue
@@ -1,7 +1,178 @@
-<template></template>
+<template>
+	<div
+		v-if="!showField && !settings.fieldOnly"
+		class="d-inline-flex align-center justify-center"
+	>
+		<span
+			class="inline-field-value pb-1 d-inline-flex align-center justify-center"
+			:class="fieldDisplayClass"
+			:style="fieldDisplayStyle"
+			@click="toggleField"
+		>
+			{{ value }}
+		</span>
+	</div>
+
+	<div
+		v-else
+		:class="fieldContainerClass"
+	>
+		<v-textarea
+			v-bind="$attrs"
+			v-model="modelValue"
+			:auto-grow="settings.autoGrow"
+			:autofocus="!settings.fieldOnly || settings.autofocus"
+			class="my-2"
+			:color="settings.color"
+			:density="settings.density"
+			:disabled="loading"
+			:error="error"
+			:hide-details="settings.hideDetails"
+			:label="settings.label"
+			:loading="loading"
+			:variant="settings.variant"
+			width="100%"
+			@keyup.enter="saveValue"
+			@keyup.esc="closeField"
+		>
+			<!-- Pass on all scoped slots -->
+			<template
+				v-for="(_, slot) in slots"
+				#[slot]="scope"
+			>
+				<slot
+					:name="slot"
+					v-bind="{ ...scope }"
+				/>
+			</template>
+
+			<template
+				v-if="!slots.append"
+				#append
+			>
+				<SaveFieldButtons
+					:cancel-icon="settings.cancelIcon"
+					:field-only="settings.fieldOnly"
+					:hide-save-icon="settings.hideSaveIcon"
+					:loading="loading"
+					:loading-icon="settings.loadingIcon"
+					:loading-icon-color="settings.loadingIconColor"
+					:save-icon="settings.saveIcon"
+					:save-icon-color="settings.saveIconColor"
+					@close="closeField"
+					@save="saveValue"
+				/>
+			</template>
+		</v-textarea>
+	</div>
+</template>
 
 <script setup lang="ts">
+import {
+	FieldValue,
+	TimeOpened,
+	UseSaveValue,
+	VInlineTextareaProps,
+} from '@/types';
+import { textareaProps } from './utils/props';
+import { SaveFieldButtons } from './components/index';
+import {
+	useSaveValue,
+	useToggleField,
+} from './composables/methods';
+import {
+	useFieldContainerClass,
+	useDisplayContainerClass,
+} from './composables/classes';
+import { useFieldDisplayStyles } from './composables/styles';
 
+import {
+	inlineEmits,
+	// inlineWatch,
+} from './shared';
+
+
+const modelValue = defineModel<FieldValue>();
+
+const value = computed(() => {
+	if (modelValue.value) {
+		empty.value = false;
+		return modelValue.value;
+	}
+
+	empty.value = true;
+	return settings.emptyText;
+});
+
+const attrs = useAttrs();
+const slots = useSlots();
+const emit = defineEmits([...inlineEmits]);
+const props = withDefaults(defineProps<VInlineTextareaProps>(), { ...textareaProps });
+let settings = reactive({ ...attrs, ...props });
+
+const empty = ref<boolean>(false);
+const error = ref<boolean>(false);
+const loading = ref<boolean>(false);
+const showField = ref<boolean>(false);
+const timeOpened = ref<TimeOpened>(null);
+let originalValue = modelValue.value;
+
+
+const fieldContainerClass = computed(() => useFieldContainerClass('textarea'));
+const fieldDisplayClass = computed(() => useDisplayContainerClass(
+	'textarea',
+	settings.valueColor,
+	settings.disabled,
+	error.value,
+	empty.value,
+));
+const fieldDisplayStyle = computed(() => useFieldDisplayStyles(
+	settings.underlineColor,
+	settings.underlineStyle,
+	settings.underlineWidth,
+	settings.color,
+	error.value,
+	settings.underlined,
+));
+
+function closeField() {
+	modelValue.value = originalValue;
+	toggleField();
+}
+
+function toggleField() {
+	if (settings.disabled) {
+		return;
+	}
+
+	const response = useToggleField(
+		settings.item.id as number,
+		showField.value,
+		attrs,
+		props,
+		timeOpened.value,
+		settings.closeSiblings,
+		settings.fieldOnly,
+	);
+
+	settings = { ...settings, ...response.settings };
+	showField.value = response.showField;
+	timeOpened.value = response.timeOpened;
+}
+
+function saveValue() {
+	originalValue = modelValue.value;
+	loading.value = true;
+	emit('loading', loading.value);
+
+	useSaveValue(settings, emit as keyof UseSaveValue, settings.name, modelValue.value as keyof UseSaveValue)
+		.then((response) => {
+			error.value = response?.error as boolean ?? false;
+			loading.value = false;
+			emit('loading', loading.value);
+			toggleField();
+		});
+}
 </script>
 
 <style lang="scss" scoped>
