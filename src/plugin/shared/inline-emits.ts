cd23c8433208ac44284d58b72279b9a2efcce369
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-05-27 16:29:03 -0700

    Removed old file

--- src/plugin/shared/inline-emits.ts
+++
@@ -1,6 +0,0 @@
-export default [
-	'loading', 						// ? emits boolean value
-	'error',							// ? emits boolean value
-	'update', 						// ? emits api response object
-	'update:model-value', // ? emits updated model value
-];
