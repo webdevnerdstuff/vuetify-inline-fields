0c9b0d7d9972f56adfe3ad3945b0eac30ca5701d
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-05-27 20:40:35 -0700

    Adding fa

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
+import '@/Libraries/fontawesome';
 import App from './App.vue';
 import CodeBlock from 'vue3-code-block';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { registerPlugins } from './plugins';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 const app = createApp(App);
 app.use(CodeBlock);
 app.use(createPinia());
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.component('FaIcon', FontAwesomeIcon);
 
 registerPlugins(app);
 
