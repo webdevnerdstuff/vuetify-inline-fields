2795ec334bfeb5ca663ecf5675cd5793944e5451
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-05-30 19:59:21 -0700

    Formatting

--- src/plugin/utils/emits.ts
+++ src/plugin/utils/emits.ts
@@ -1,7 +1,7 @@
 export default [
-	'error',										// ? emits boolean value
-	'loading', 									// ? emits boolean value
-	'update', 									// ? emits api response object
-	'update:closeSiblingFields', // ? emits boolean value
-	'update:model-value', 			// ? emits updated model value
+	'error',											// ? emits boolean value
+	'loading', 										// ? emits boolean value
+	'update', 										// ? emits api response object
+	'update:closeSiblingFields', 	// ? emits boolean value
+	'update:model-value', 				// ? emits updated model value
 ];
