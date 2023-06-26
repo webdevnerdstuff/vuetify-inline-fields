f2c7cbbc5118b7aff8214bcd708b226d4d6d5ce6
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-06-25 17:14:44 -0700

    Changing to mdi

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,9 +3,9 @@ import '@mdi/font/css/materialdesignicons.css';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import defaultThemes from './theme';
-import { mdi } from 'vuetify/iconsets/mdi';
 import { createVuetify } from 'vuetify';
-import { aliases, fa } from 'vuetify/iconsets/fa-svg';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import { fa } from 'vuetify/iconsets/fa-svg';
 
 // ! This is Labs workaround, need to update later //
 import * as labs from 'vuetify/labs/components';
@@ -19,7 +19,7 @@ export default createVuetify({
 	directives,
 	icons: {
 		aliases,
-		defaultSet: 'fa',
+		defaultSet: 'mdi',
 		sets: {
 			fa,
 			mdi,
