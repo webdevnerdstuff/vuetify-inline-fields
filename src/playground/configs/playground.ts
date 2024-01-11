8fce3dddde47c2c15fd36101936523efe85bc3d2
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2024-01-11 13:33:36 -0800

    Add more test examples

--- src/playground/configs/playground.ts
+++ src/playground/configs/playground.ts
@@ -12,32 +12,54 @@ makeServer({ environment: 'playground' });
 
 const app = createApp(PlaygroundApp);
 app.use(createVInlineFields({
-	cancelButtonColor: 'yellow', // ! Fail
-	// cancelButtonSize: 'x-small', // ? CONFIRMED
-	// cancelButtonTitle: 'xxxxxxxxxxx', // ? CONFIRMED
-	// cancelButtonVariant: 'outlined', // ? CONFIRMED
-	// cancelIcon: 'mdi:mdi-cog', // ? CONFIRMED
-	// cancelIconColor: 'purple', // ? CONFIRMED
-	// cardField: true, // ? CONFIRMED
-	// cardOffsetX: 100, // ? CONFIRMED
-	// cardOffsetY: 100, // ? CONFIRMED
-	// cell: true, // ? CONFIRMED
-	// cellUnderlineFullWidth: false, // ? CONFIRMED
-	// closeSiblings: false, // ? CONFIRMED
-	// color: 'orange', // * CONFIRMED
-	// disabled: false, // & not allowed
-	// emptyText: 'foobar', // ? CONFIRMED
-	// fieldOnly: true, // ? CONFIRMED
-	// hideCancelIcon: true, // ? CONFIRMED
+	// cancelButtonColor: 'red',
+	// cancelButtonSize: 'x-small',
+	// cancelButtonTitle: 'xxxxxxxxxxx',
+	// cancelButtonVariant: 'outlined',
+	// cancelIcon: 'mdi:mdi-cog',
+	// cancelIconColor: 'purple',
+	// cardField: true,
+	// cardOffsetX: 100,
+	// cardOffsetY: 100,
+	// cell: true,
+	// cellUnderlineFullWidth: false,
+	// closeSiblings: false,
+	// color: 'orange',
+	// displayAppendIcon: 'mdi:mdi-pencil',
+	// displayAppendIconColor: 'yellow',
+	// displayAppendIconSize: 'x-large',
+	// displayAppendInnerIcon: 'mdi:mdi-pencil',
+	// displayAppendInnerIconColor: 'blue',
+	// displayAppendInnerIconSize: 'x-large',
+	// displayPrependIcon: 'mdi:mdi-pencil',
+	// displayPrependIconColor: 'red',
+	// displayPrependIconSize: 'x-large',
+	// displayPrependInnerIcon: 'mdi:mdi-pencil',
+	// displayPrependInnerIconColor: 'green',
+	// displayPrependInnerIconSize: 'x-large',
+	// emptyText: 'foobar',
+	// falseValue: 'mike',
+	// fieldOnly: true,
+	// hideCancelIcon: true,
 	// hideDetails: true,
-	// hideSaveIcon: true, // ? CONFIRMED
-	// loadingWait: false,  // ? CONFIRMED
+	// hideSaveIcon: true,
+	// icons: false,
+	// iconFalse: 'mdi:mdi-minus',
+	// iconFalseColor: 'orange',
+	// iconFalseTitle: 'nope',
+	// iconTrue: 'mdi:mdi-plus',
+	// iconTrueColor: 'yellow',
+	// iconTrueTitle: 'yup',
+	// loadingWait: false,
+	// saveButtonColor: 'red',
+	// saveIcon: 'mdi:mdi-cog',
 	// tableField: false,
-	// underlineColor: 'red',  // ? CONFIRMED
-	// underlineStyle: 'dashed',  // ? CONFIRMED
-	// underlineWidth: '10px',  // ? CONFIRMED
-	// underlined: false,  // ? CONFIRMED
-	// valueColor: 'pink',  // ? CONFIRMED
+	// trueValue: 'bob',
+	// underlineColor: 'red',
+	// underlineStyle: 'dashed',
+	// underlineWidth: '10px',
+	// underlined: false,
+	// valueColor: 'pink',
 }));
 app.use(createPinia());
 app.component('font-awesome-icon', FontAwesomeIcon);
